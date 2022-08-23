082d5c2d7d39076e0752bbb21fdfa96850fec645
Subhajit Pal <subhajitpal@Subhajits-MacBook-Air-2.local>
2022-08-24 01:12:22 +0530

    Setting up Stripe Payment method

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 import App from './App';
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe';
 import './index.scss';
 
 
@@ -13,7 +15,9 @@ ReactDOM.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-            <App />    
+          <Elements stripe={stripePromise}>
+            <App /> 
+          </Elements>       
         </BrowserRouter>
       </PersistGate>
     </Provider>
